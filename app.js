0b37ce588dab9f320e48c3f3a64706f4e4977fe5
joannaWebDev <joannasmerea@gmail.com>
2020-10-06 21:08:09 +0200

    finished

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const URL =
-  "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
-
-/*
-fetch('url')
-.then(Resolve)
-.then(Reject) OR .catch(Reject)
-
-OR directly 
-
-fetch('url').then(ResolveFuntion, RejectFuntion)
-
-*/
-
+const URL = "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
 const app = document.querySelector("#app");
+const interval = 1000;
+const columnQuantity = 4;
+const getPhotos = URL => {
+  fetch(URL)
+    .then(response => onSuccessResponse(response))
+    .catch(error => onErrorResponse(error));
+};
+const createImg = photo => {
+  const img = document.createElement("img");
+  img.setAttribute("src", photo.url);
+  return img;
+};
+const createColumn = () => {
+  const divColumn = document.createElement("div");
+  divColumn.classList = "column";
+  return divColumn;
+};
+const createRow = () => {
+  const divRow = document.createElement("div");
+  divRow.classList = "row";
+  return divRow;
+};
+const onSuccessResponse = response =>
+  response.json().then(photos => {
+    let row;
+    for (const photoIndex in photos) {
+      setTimeout(() => {
+        const column = createColumn();
+        const img = createImg(photos[photoIndex]);
+        if (photoIndex % columnQuantity === 0) row = createRow();
+        row.appendChild(column);
+        column.appendChild(img);
+        app.appendChild(row);
+      }, interval * photoIndex);
+    }
+  });
+const onErrorResponse = error => console.error("Aqui esta el error", error);
+getPhotos(URL);
